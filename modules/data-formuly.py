233a275afab174dc60b8d3c45730ced1b7f6a004
jorge garcia <jorgearmando1999@hotmail.com>
2021-07-12 14:43:51 -0700

    Agregacion de documento de consulta con datos

--- modules/data-formuly.py
+++ modules/data-formuly.py
@@ -1,3 +1,4 @@
 Usuarios  = [[Jose,Gerardo,Luis],[18,20,19],[ejemplo1@correo.com,ejempl2@correo.com,],[]] Usuarios  = [[Jose,Gerardo,Luis],[18,20,19],[ejemplo1@correo.com,ejempl2@correo.com,ejempl3@correo.com],[H,H,H]];
 Fecha = [02/07/2021,04/05/2021,06/08/2021];
 ID =[5,2,4];
+Cuestionarios=[comida,historio,cultural];
