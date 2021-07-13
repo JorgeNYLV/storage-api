d84a5fbf16d7c4e806a6b47de372225374189ade
jorge garcia <jorgearmando1999@hotmail.com>
2021-07-13 07:32:01 -0700

    Correccion de descripciones y de get

--- routes/url-messa.py
+++ routes/url-messa.py
@@ -5,19 +5,25 @@ import bottle
 app = bottle.Bottle()
 
 @app.post("/encuesta")
-def store_record(*args, **kwargs):
+def get_encues(*args, **kwargs):
     bottle.response.status = 501
     bottle.response.content_type = "application/json"
     return dict(code= 501, message = "Not implemented ")
 
-@app.get("/consulta")
-def get_all_info(*args, **kwargs):
+@app.get("/encuesta<User>")
+def get_encuesta_User(*args, **kwargs):
     bottle.response.status = 501
     bottle.response.content_type = "application/json"
     return dict(code = 501, message = "Not implemented")
 
-@app.get("/historial")
-def get_info_by_sn(*args, **kwargs):
+@app.get("/encuesta<ID>")
+def get_encuesta_ID(*args, **kwargs):
+    bottle.response.status = 501
+    bottle.response.content_type = "application/json"
+    return dict(code = 501, message = "Not implemented")
+
+@app.get("/encuesta<Delete>")
+def get_encuesta_delete(*args, **kwargs):
     bottle.response.status = 501
     bottle.response.content_type = "application/json"
     return dict(code = 501, message = "Not implemented")
