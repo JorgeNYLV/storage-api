3c1976bea5f67e4ef28b94a1db191eebcbb3aabf
jorge garcia <jorgearmando1999@hotmail.com>
2021-07-12 17:19:08 -0700

    Coreecion de array de consulta

--- modules/data-formuly.py
+++ modules/data-formuly.py
@@ -1,4 +1,10 @@
-Usuarios  = [[Jose,Gerardo,Luis],[18,20,19],[ejemplo1@correo.com,ejempl2@correo.com,],[]] Usuarios  = [[Jose,Gerardo,Luis],[18,20,19],[ejemplo1@correo.com,ejempl2@correo.com,ejempl3@correo.com],[H,H,H]];
+import array
+Usuarios  = [Jose,Gerardo,Luis];
+Edad =[18,20,19];
+Correo =[ejemplo1@correo.com,ejempl2@correo.com,ejempl3@correo.com];
+Genero =[H,H,H]];
 Fecha = [02/07/2021,04/05/2021,06/08/2021];
 ID =[5,2,4];
 Cuestionarios=[comida,historio,cultural];
+ b=array.array("Usuarios",[0],"Edad",[0],"Correo",[0],"Genero",[0],"Fecha",[0],"ID",[0],)
+ 
